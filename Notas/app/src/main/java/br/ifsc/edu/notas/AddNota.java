1dbd9fda230b505b643d887858c3d1a171051bd9
Raquel Cristina <isenseeraquel@gmail.com>
2019-12-06 10:20:00 -0300

    add new note

---
+++ Notas/app/src/main/java/br/ifsc/edu/notas/AddNota.java
@@ -0,0 +1,54 @@
+package br.ifsc.edu.notas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddNota extends AppCompatActivity {
+    EditText titulo, texto;
+    Button add;
+    SQLiteDatabase bd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_nota);
+
+        add = findViewById(R.id.btnAdd);
+        titulo = findViewById(R.id.txtTitulo);
+        texto = findViewById(R.id.txtTexto);
+
+        bd = openOrCreateDatabase("database", MODE_PRIVATE, null);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tituloS = titulo.getText().toString();
+                String textoS = texto.getText().toString();
+
+                Insert(tituloS, textoS);
+
+                Toast.makeText(AddNota.this, "Nota adicionada!", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(AddNota.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void Insert(String titulo, String texto) {
+        //INSERT
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("titulo", titulo);
+        contentValues.put("texto", texto);
+        bd.insert("notas", null, contentValues);
+    }
+}
--- Notas/app/src/main/java/br/ifsc/edu/notas/MainActivity.java
+++ Notas/app/src/main/java/br/ifsc/edu/notas/MainActivity.java
@@ -3,6 +3,7 @@ package br.ifsc.edu.notas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -10,8 +11,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -20,46 +25,61 @@ public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase bd;
     ListView notas;
-
+    Button novaNota;
+    String titulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bd = openOrCreateDatabase("database",MODE_PRIVATE,null);
+        bd = openOrCreateDatabase("database", MODE_PRIVATE, null);
         notas = findViewById(R.id.listNotas);
+        novaNota = findViewById(R.id.btnNovaNota);
 
+        novaNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddNota.class);
+                startActivity(intent);
+
+            }
+        });
+
+        notas.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                titulo = novaNota.getText().toString();
+                bd.execSQL("DELETE FROM notas WHERE titulo = 'titulo'");
+
+                Toast.makeText(getApplicationContext(), "Nota removida", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+        });
 
 //CREATE TABLE
         bd.execSQL("CREATE TABLE IF NOT EXISTS notas (" +
-                  "id integer primary key autoincrement," +
-                  "titulo varchar not null," +
-                  "texto varchar );");
-
-//INSERT
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("titulo","Segunda Nota");
-        contentValues.put("texto","segundo texto");
-        bd.insert("notas", null,contentValues);
+                "id integer primary key autoincrement," +
+                "titulo varchar not null," +
+                "texto varchar );");
 
 
-//SELECT
-        Cursor  cursor = bd.rawQuery("SELECT * FROM notas", null,null);
+        //SELECT
+        Cursor cursor = bd.rawQuery("SELECT * FROM notas", null, null);
         cursor.moveToFirst();
 
         String id, titulo, texto;
         final ArrayList<String> arrayList = new ArrayList<>();
 
         while (!cursor.isAfterLast()) {
-//            id = cursor.getString(cursor.getColumnIndex("id"));
             titulo = cursor.getString(cursor.getColumnIndex("titulo"));
-//            texto = cursor.getString(cursor.getColumnIndex("texto"));
             cursor.moveToNext();
             arrayList.add(titulo);
         }
 
-//LIST
+
+        //LIST
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 getApplicationContext(),
                 android.R.layout.simple_list_item_1,
@@ -74,9 +94,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String s = arrayList.get(i);
-                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
             }
         });
 
     }
+
 }
+
